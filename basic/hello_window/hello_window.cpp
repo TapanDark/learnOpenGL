1076bf042857332164aeb7070b8d17582416d140
Tapan Adarkar <tadarkar@nvidia.com>
2019-10-19 16:11:12 +0530

    WIP: Hello Window: Improvements and Notes

--- basic/hello_window/hello_window.cpp
+++ basic/hello_window/hello_window.cpp
@@ -1,12 +1,20 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+//Callback methods for window resize
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+//Method to process input, if any.
+void processInput(GLFWwindow *window);
+
+const unsigned int WIDTH = 800;
+const unsigned int HEIGHT = 600;
+
 int main(void)
 {
     GLFWwindow *window;
 
     std::cout << "Initializing GLFW" << std::endl;
-    /* Initialize the library */
+    // Initialize and configure GLFW
     if (!glfwInit())
     {
         std::cout << "GLFW INIT Failed" << std::endl;
@@ -17,8 +25,8 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World", NULL, NULL);
+    if (window == NULL)
     {
         std::cout << "Failed to create window!" << std::endl;
         glfwTerminate();
@@ -27,11 +35,17 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    // set callback to resize our GL framebuffer upon window resize
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        //perform actions on input
+        processInput(window);
+
         /* Render here */
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
@@ -44,4 +58,20 @@ int main(void)
     std::cout << "Goodbye world!" << std::endl;
     glfwTerminate();
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    // match our GL viewport size to window dimensions.
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    // set close intent if ESCAPE is pressed, nice.
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        std::cout<<"ESCAPE KEY PRESSED!"<<std::endl;
+        glfwSetWindowShouldClose(window, true);
+    }
 }
