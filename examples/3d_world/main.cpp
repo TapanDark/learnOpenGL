610332cf17900da42370d823133a62448e0846e4
Tapan Adarkar <tapan.dark@gmail.com>
2019-11-10 22:30:47 +0530

    Instanced data for 2 cubes
    -This broke aspect ratio adjustment on window resize.
     TODO: Fix this.

--- examples/3d_world/main.cpp
+++ examples/3d_world/main.cpp
@@ -36,6 +36,14 @@ int main(int argc, char *argv[])
     }
     Shape cube = ShapeGenerator::makeCube();
 
+    //create our transformation matrices:
+    glm::mat4 projectionMatrix = glm::perspective(glm::radians(60.0f), ((float)curr_width / (float)curr_height), 0.1f, 10.0f);
+    glm::mat4 fullTransformationMatrices[] = {
+        glm::translate(projectionMatrix, glm::vec3(-1.0f, 0.0f, -3.0f)) *
+            glm::rotate(glm::mat4(1.0f), glm::radians(54.0f), glm::vec3(1.0f, 1.0f, 0.0f)),
+        glm::translate(projectionMatrix, glm::vec3(1.0f, 0.0f, -3.0f)) *
+            glm::rotate(glm::mat4(1.0f), glm::radians(120.0f), glm::vec3(1.0f, 1.0f, 0.0f))};
+
     //enable depth test
     glEnable(GL_DEPTH_TEST);
 
@@ -47,29 +55,50 @@ int main(int argc, char *argv[])
     unsigned int EBO;
     glGenBuffers(1, &EBO);
 
-    //Create a veretex array object to store our svertex attribute configurations.
+    //create element buffer to store our transformation matrices
+    unsigned int TBO;
+    glGenBuffers(1, &TBO);
+
+    //Create a veretex array object to store our vertex attribute configurations.
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
 
-    //Bind buffer to GL_ARRAY
+    //copy our vertex data to buffer
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-    //copy our data to buffer
     glBufferData(GL_ARRAY_BUFFER, cube.vertexBufferSize(), cube.getVertices(), GL_STATIC_DRAW);
 
-    //Bind element buffer to GL_ARRAY
+    //copy our element data to buffer
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-
-    //copy our data to buffer
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, cube.indexBufferSize(), cube.getIndices(), GL_STATIC_DRAW);
 
-    //Vertex attribute setup
+    //Vertex attributes setup
+    //position attribute
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
 
+    //vColor attribute
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(sizeof(float) * 3));
+
+    //copy our transformation matrix instanced data to buffer
+    glBindBuffer(GL_ARRAY_BUFFER, TBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(fullTransformationMatrices), fullTransformationMatrices, GL_STATIC_DRAW);
+
+    //transform matrix attribute
+    glEnableVertexAttribArray(2);
+    glEnableVertexAttribArray(3);
+    glEnableVertexAttribArray(4);
+    glEnableVertexAttribArray(5);
+    glVertexAttribPointer(2, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void *)(sizeof(float) * 0));
+    glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void *)(sizeof(float) * 4));
+    glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void *)(sizeof(float) * 8));
+    glVertexAttribPointer(5, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void *)(sizeof(float) * 12));
+    //setup divisor 1 to use once per render instance
+    glVertexAttribDivisor(2, 1);
+    glVertexAttribDivisor(3, 1);
+    glVertexAttribDivisor(4, 1);
+    glVertexAttribDivisor(5, 1);
 
     //create and compile our shaders
     ShaderObject *vertexShader, *fragmentShader;
@@ -87,7 +116,6 @@ int main(int argc, char *argv[])
     while (windowObj.isOpen())
     {
         /* Render here */
-        // glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -95,27 +123,7 @@ int main(int argc, char *argv[])
         myShaderProgram.useProgram();
         glBindVertexArray(VAO);
 
-        glm::mat4 translationMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(-1.0f, 0.0f, -3.0f));
-        glm::mat4 rotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(54.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-        glm::mat4 rotation2Matrix = glm::rotate(glm::mat4(1.0f), glm::radians(54.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-        glm::mat4 projectionMatrix = glm::perspective(glm::radians(60.0f), ((float)curr_width / (float)curr_height), 0.1f, 10.0f);
-
-        glm::mat4 fullTransformMatrix = projectionMatrix * translationMatrix * rotationMatrix * rotation2Matrix;
-
-        myShaderProgram.SetUniform(myShaderProgram.GetUniform("fullTransformMatrix"), &fullTransformMatrix, 1);
-
-        glDrawElements(GL_TRIANGLES, cube.getNumVertices(), GL_UNSIGNED_SHORT, 0);
-
-        translationMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(1.0f, 0.0f, -3.0f));
-        rotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(-54.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-        rotation2Matrix = glm::rotate(glm::mat4(1.0f), glm::radians(-54.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-        projectionMatrix = glm::perspective(glm::radians(60.0f), ((float)curr_width / (float)curr_height), 0.1f, 10.0f);
-
-        fullTransformMatrix = projectionMatrix * translationMatrix * rotationMatrix * rotation2Matrix;
-
-        myShaderProgram.SetUniform(myShaderProgram.GetUniform("fullTransformMatrix"), &fullTransformMatrix, 1);
-
-        glDrawElements(GL_TRIANGLES, cube.getNumVertices(), GL_UNSIGNED_SHORT, 0);
+        glDrawElementsInstanced(GL_TRIANGLES, cube.getNumVertices(), GL_UNSIGNED_SHORT, 0, 2);
 
         //Actuall swap buffers so our new image is visible
         windowObj.refresh();
--- examples/3d_world/shaders/vertex.glsl
+++ examples/3d_world/shaders/vertex.glsl
@@ -1,9 +1,7 @@
 #version 330 core
 layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 vColor;
-
-uniform mat4 modelTransformMatrix;
-uniform mat4 fullTransformMatrix;
+layout (location = 2) in mat4 fullTransformMatrix;
 
 out vec3 outColor;
 
